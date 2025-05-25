fb0e5c5948aef60d896f6577ea42d9dc8276ad2f
Ashlin Asokan <simpleashlin@gmail.com>
2025-05-25 03:01:43 -0400

    Added Cloudflare Workers

--- src/lib/sendMail.ts
+++ src/lib/sendMail.ts
@@ -1,9 +1,10 @@
 'use server';
 
+import { env } from 'process';
 import { Resend } from 'resend';
 export async function sendMail(name: string, email: string, message: string) {
   try {
-    const resend = new Resend(process.env.RESEND_API_KEY);
+    const resend = new Resend(env.RESEND_API_KEY);
     resend.domains.create({name: "email.ashlin.dev"})
 
     const body = `<html>Hey, <strong>${name}</strong> (${email}) has messaged you this:
