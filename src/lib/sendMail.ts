d0eb13ad3280300fb0dccd04539b6b73e06ff557
Ashlin Asokan <simpleashlin@gmail.com>
2025-05-25 21:39:02 -0400

    Fixed env variables

--- src/lib/sendMail.ts
+++ src/lib/sendMail.ts
@@ -1,16 +1,16 @@
 'use server';
 
-import { env } from 'process';
 import { Resend } from 'resend';
 export async function sendMail(name: string, email: string, message: string) {
   try {
-    const resend = new Resend(env.RESEND_API_KEY);
-    resend.domains.create({name: "email.ashlin.dev"})
-
+    const resend = new Resend(process.env.RESEND_API_KEY);
+    if (!process.env.RESEND_API_KEY) {
+      throw new Error('RESEND_API_KEY is not set');
+    }
     const body = `<html>Hey, <strong>${name}</strong> (${email}) has messaged you this:
     <p>${message}</p></html>`;
 
-    resend.emails.send({
+    await resend.emails.send({
       from: 'portfolio@email.ashlin.dev',
       to: 'code@ashlin.dev',
       subject: 'Portfolio Contact Form',
@@ -18,7 +18,7 @@ export async function sendMail(name: string, email: string, message: string) {
     });
     return true
   } catch (error) {
-    console.log(error);
+    console.error('Failed to send Email:', error);
     return false
   }
 }
