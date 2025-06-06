12d1f534660c88b79c656ed7f20b2bdd39ef6f7a
Ashlin Asokan <simpleashlin@gmail.com>
2025-06-06 00:10:52 -0400

    Improved Skills Section

--- src/components/ProgrammingSkillSection.tsx
+++ src/components/ProgrammingSkillSection.tsx
@@ -20,14 +20,43 @@ import {
   SiPostgresql,
   SiFlask,
   SiTailwindcss,
-  SiHono
+  SiHono,
 } from 'react-icons/si';
 import { DiJava } from 'react-icons/di';
 import TechExpItem from '@/components/TechExpItem';
 
+const listItems = [
+  { text: 'Javascript', icon: SiJavascript },
+  { text: 'Typescript', icon: SiTypescript },
+  { text: 'Python', icon: SiPython },
+  { text: 'Java', icon: DiJava },
+  { text: 'C Lang', icon: SiC },
+  { text: 'HTML', icon: SiHtml5 },
+  { text: 'CSS', icon: SiCss3 },
+  { text: 'Bootstrap', icon: SiBootstrap },
+  { text: 'Tailwind', icon: SiTailwindcss },
+  { text: 'React', icon: SiReact },
+  { text: 'NextJs', icon: SiNextdotjs },
+  { text: 'NodeJS', icon: SiNodedotjs },
+  { text: 'Express', icon: SiExpress },
+  { text: 'Flask', icon: SiFlask },
+  { text: 'Hono', icon: SiHono },
+  { text: 'MongoDB', icon: SiMongodb },
+  { text: 'Mongoose', icon: SiMongoose },
+  { text: 'MySQL', icon: SiMysql },
+  { text: 'PostgreSQL', icon: SiPostgresql },
+  { text: 'Prisma ORM', icon: SiPrisma },
+  { text: 'Docker', icon: SiDocker },
+  { text: 'Cloudflare Workers', icon: SiCloudflareworkers },
+  { text: 'Cloudflare Pages', icon: SiCloudflarepages },
+];
+
 const ProgrammingSkillSection = () => {
   return (
-    <section className="p-4 sm:p-6 md:p-10 lg:p-20 text-dark-txt bg-secondary" aria-label="Programming Skills">
+    <section
+      className="p-4 sm:p-6 md:p-10 lg:p-20 text-dark-txt bg-secondary"
+      aria-label="Programming Skills"
+    >
       <div className="grid grid-cols-1 intersect-once intersect:motion-preset-slide-up lg:grid-cols-3 gap-6 max-w-7xl mx-auto items-center">
         <div className="flex justify-center lg:justify-start">
           <img
@@ -38,36 +67,18 @@ const ProgrammingSkillSection = () => {
           />
         </div>
         <div className="p-4 sm:p-6 md:p-8 lg:p-10 flex flex-col gap-4 md:gap-5 lg:col-span-2">
-          <h3 className="font-bold text-3xl sm:text-4xl md:text-5xl">Programming Skills</h3>
+          <h3 className="font-bold text-3xl sm:text-4xl md:text-5xl">
+            Programming Skills
+          </h3>
           <p className="font-light text-lg sm:text-xl md:text-2xl">
             I specialize in modern frameworks and tools to build fast,
             user-friendly websites and apps. Here’s the tech I work with to
             create smooth experiences and reliable solutions.
           </p>
           <div className="flex gap-2 sm:gap-2 flex-wrap justify-center lg:justify-start">
-            <TechExpItem text="Javascript" icon={SiJavascript} />
-            <TechExpItem text="Typescript" icon={SiTypescript} />
-            <TechExpItem text="Python" icon={SiPython} />
-            <TechExpItem text="Java" icon={DiJava} />
-            <TechExpItem text="C Lang" icon={SiC} />
-            <TechExpItem text="HTML" icon={SiHtml5} />
-            <TechExpItem text="CSS" icon={SiCss3} />
-            <TechExpItem text="Bootstrap" icon={SiBootstrap} />
-            <TechExpItem text="Tailwind" icon={SiTailwindcss} />
-            <TechExpItem text="React" icon={SiReact} />
-            <TechExpItem text="NextJs" icon={SiNextdotjs} />
-            <TechExpItem text="NodeJS" icon={SiNodedotjs} />
-            <TechExpItem text="Express" icon={SiExpress} />
-            <TechExpItem text="Flask" icon={SiFlask} />
-            <TechExpItem text="Hono" icon={SiHono} />
-            <TechExpItem text="MongoDB" icon={SiMongodb} />
-            <TechExpItem text="Mongoose" icon={SiMongoose} />
-            <TechExpItem text="MySQL" icon={SiMysql} />
-            <TechExpItem text="PostgreSQL" icon={SiPostgresql} />
-            <TechExpItem text="Prisma ORM" icon={SiPrisma} />
-            <TechExpItem text="Docker" icon={SiDocker} />
-            <TechExpItem text="Cloudflare Workers" icon={SiCloudflareworkers} />
-            <TechExpItem text="Cloudflare Pages" icon={SiCloudflarepages} />
+            {listItems.map((item, index) => (
+              <TechExpItem key={index} text={item.text} icon={item.icon} />
+            ))}
           </div>
         </div>
       </div>
